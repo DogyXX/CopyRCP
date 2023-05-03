b68a5c33e2039488d67b79f1b82575bede6e48d8
wisniax <wisnia20012@gmail.com>
2023-05-03 21:27:10 +0200

    Init Commit

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverControlApp
+{
+	public class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("JD");
+		}
+	}
+}
